f892d41dd408f79585bf2f5e37c5b18e30e1c364
UGONT <134973209+UGONT@users.noreply.github.com>
2024-04-23 14:00:41 -0400

    Java

---
+++ formularios/func.js
@@ -0,0 +1,15 @@
+function validar(){
+
+    const user = document.getElementById("user").value;
+    const pass = document.getElementById("pass").value;
+    if(String(user).length >= 5 && String(user).length <= 30){
+        if(String(pass).length >=8){
+            document.getElementById("resultado").innerHTML="<div class='alert alert-success mx-auto'>hola mundo</div>"
+        }else{
+            document.getElementById("resultado").innerHTML="<div class='alert alert-danger mx-auto'>Contraseña no valida</div>"
+        }
+    }else{
+        document.getElementById("resultado").innerHTML="<div class='alert alert-danger mx-auto'>Usuario no valido</div>"
+    }
+    
+}
