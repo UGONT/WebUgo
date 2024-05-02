c4213eb1592c213ca4a27eae5d877303c3538861
UGONT <134973209+UGONT@users.noreply.github.com>
2024-05-01 21:30:47 -0400

    We

--- ActForm/validar.js
+++ ActForm/validar.js
@@ -5,16 +5,42 @@ $(document).ready(function () {
     })
 
     $("#btnGenerar").click(function () {
-        let rut = $("#txtRut").val;
+        let rut = $("#txtRut").val();
+        let nombre = $("#txtNombre").val();
+        let apellidoPat = $("#txtApellidoPat").val();
+        let apellidoMat = $("#txtApellidoMat").val();
+        let edad = $("#txtEdad").val();
+        let valorSeleccionado = $("#optGenero").val();
+        let textoSeleccionado = $("#optGenero").find("option:selected").text();
+        
+        let res = validar(rut,nombre,apellidoPat,apellidoMat,edad,valorSeleccionado);
+        if(res){
+            $("#estado").html("<div class='alert alert-success w-50 mx-auto text-center' >WEna</div>");
+        };
         
     })
     
-    function validar(rut){
+    function validar(rut,nombre,apellidoPat,apellidoMat,edad,valorSeleccionado){
         if (String(rut).length < 9 || String(rut).length > 10 ) {
-            
+            $("#estado").html("<div class='alert alert-danger w-50 mx-auto text-center' >Rut debe tener largo entre 9 y 10 caracteres.</div>");
+   
+        }else if (String(nombre).length < 3 || String(nombre).length > 20)  {
+            $("#estado").html("<div class='alert alert-danger w-50 mx-auto text-center' >Nombre debe tener largo entre 3 y 20 caracteres.</div>");
+
+        }else if (String(apellidoPat).length < 3 || String(apellidoPat).length > 20)  {
+            $("#estado").html("<div class='alert alert-danger w-50 mx-auto text-center' >Apellido Paterno debe tener largo entre 3 y 20 caracteres.</div>");
+
+        }else if (String(apellidoMat).length < 3 || String(apellidoMat).length > 20)  {
+            $("#estado").html("<div class='alert alert-danger w-50 mx-auto text-center' >Apellido Materno debe tener largo entre 3 y 20 caracteres.</div>");
+        
+        }else if (Number(edad) < 18 || Number(edad) > 35) {
+            $("#estado").html("<div class='alert alert-danger w-50 mx-auto text-center' >Edad debe ser entre 18 y 35 años.</div>");
+        
+        }else if (Number(valorSeleccionado) == 0) {
+            $("#estado").html("<div class='alert alert-danger w-50 mx-auto text-center' >Debes seleccionar un genero.</div>");
         }
-        else if (condition) {
-            
+        else{
+            return true;
         }
     };
-})
+});
