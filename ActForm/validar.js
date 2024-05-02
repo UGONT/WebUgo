8a20c7969e1c6f4e301149f8e787133f556c7206
UGONT <134973209+UGONT@users.noreply.github.com>
2024-05-01 23:07:26 -0400

    Update validar.js

--- ActForm/validar.js
+++ ActForm/validar.js
@@ -12,15 +12,17 @@ $(document).ready(function () {
         let edad = $("#txtEdad").val();
         let valorSeleccionado = $("#optGenero").val();
         let textoSeleccionado = $("#optGenero").find("option:selected").text();
-        
-        let res = validar(rut,nombre,apellidoPat,apellidoMat,edad,valorSeleccionado);
+        let celular = $("#txtCelular").val();
+        console.log(celular);
+        let res = validar(rut,nombre,apellidoPat,apellidoMat,edad,valorSeleccionado,celular);
         if(res){
             $("#estado").html("<div class='alert alert-success w-50 mx-auto text-center' >WEna</div>");
+            
         };
         
     })
     
-    function validar(rut,nombre,apellidoPat,apellidoMat,edad,valorSeleccionado){
+    function validar(rut,nombre,apellidoPat,apellidoMat,edad,valorSeleccionado,celular){
         if (String(rut).length < 9 || String(rut).length > 10 ) {
             $("#estado").html("<div class='alert alert-danger w-50 mx-auto text-center' >Rut debe tener largo entre 9 y 10 caracteres.</div>");
    
@@ -38,6 +40,9 @@ $(document).ready(function () {
         
         }else if (Number(valorSeleccionado) == 0) {
             $("#estado").html("<div class='alert alert-danger w-50 mx-auto text-center' >Debes seleccionar un genero.</div>");
+        
+        }else if (String(celular).length < 9 || String(celular).length > 12) {
+            $("#estado").html("<div class='alert alert-danger w-50 mx-auto text-center' >Numero de celular no valido, debe ser entre 9 y 12 digitos.</div>");
         }
         else{
             return true;
