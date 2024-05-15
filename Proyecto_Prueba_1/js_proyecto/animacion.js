d40db2978c4a4790b045b916d2a3a63db1305793
UGONT <134973209+UGONT@users.noreply.github.com>
2024-05-15 00:24:27 -0400

    Web

--- Proyecto_Prueba_1/js_proyecto/animacion.js
+++ Proyecto_Prueba_1/js_proyecto/animacion.js
@@ -1,23 +1,24 @@
 barba.init({
-    transitions: [{
-      name: 'fade',
-      once(data) {
-        gsap.from(data.next.container, {
-          opacity: 0,
-          duration: 0.2
-        });
-      },
-      leave(data) {
-        return gsap.to(data.current.container, {
-          opacity: 0,
-          duration: 0.2
-        });
-      },
-      enter(data) {
-        gsap.from(data.next.container, {
-          opacity: 0,
-          duration: 0.2
-        });
-      }
-    }]
-  });
+  transitions: [{
+    name: 'fade',
+    once(data) {
+      gsap.from(data.next.container, {
+        opacity: 0,
+        duration: 0.2
+      });
+    },
+    leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 0,
+        duration: 0.2
+      });
+    },
+    enter(data) {
+      gsap.from(data.next.container, {
+        opacity: 0,
+        duration: 0.2
+      });
+    }
+  }]
+});
+
