18c5a2df0c84b42d922f85cc61fa8b4adfadd56d
UGONT <134973209+UGONT@users.noreply.github.com>
2024-05-17 13:44:56 -0400

    Web

--- Proyecto_Prueba_1/js_proyecto/icono.js
+++ Proyecto_Prueba_1/js_proyecto/icono.js
@@ -3,6 +3,7 @@ window.addEventListener('scroll', function() {
     var btnVolverArriba = document.getElementById('btnVolverArriba');
     if (window.scrollY > 100) { 
       btnVolverArriba.style.display = 'block';
+      
     } else {
       btnVolverArriba.style.display = 'none';
     }
